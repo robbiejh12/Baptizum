a1ee80beeba645277da17f4dfe651969ce4524e8
robbiejh12 <robbiejh12@users.noreply.github.com>
2017-10-02 00:22:32 +0100

    Add files via upload

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -81,7 +81,7 @@ function loadFiles(files) {
 }
 
 function processcsv(input) {
-	var songs = $.csv.toObjects(input);
-	songarray += songs;
-	console.log(songarray);
+#	var songs = $.csv.toObjects(input);
+#	songarray += songs;
+	console.log(input);
 }
